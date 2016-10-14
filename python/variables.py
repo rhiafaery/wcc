7122f146a3e8189b41ebef81af007d19d0395a3c
Tricia <chocolategrl@gmail.com>
2016-10-13 23:20:23 -0400

    variables file

---
+++ python/variables.py
@@ -0,0 +1,17 @@
+x = 4
+y = x + 1
+
+print(x)
+print(y)
+
+age = 30
+first_name = 'Ariel'
+print(first_name + ' is ' + str(age) + ' years old.')
+
+
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+print(first_name + ' is ' + str(age) + " years old now, but she'll turn " + str(next_age) + ' in August. ')
+print('Her birth stone is '+ birth_stone)
